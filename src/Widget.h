264650872b2041fb20e80f84e044910052766aac
jbbjarnason <jbbjarnason@gmail.com>
2021-08-05 14:37:37 +0000

    New feature ... Dynamic Tabs :sparkles:

---
+++ src/Widget.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+#define WIDGET_JSON_SIZE 1024
+
+struct WidgetNames {
+  int value;
+  const char* type;
+};
+
+class Tab;
+
+class Widget {
+  protected:
+    uint32_t _id;
+    String _name;
+    int   _type;
+    bool  _changed;
+
+    using JsonDocument = DynamicJsonDocument;
+
+    virtual JsonDocument generateLayout() = 0;
+    virtual JsonDocument generateUpdate() = 0;
+
+  public:
+    Widget() {
+      #if defined(ESP8266)
+        _id = RANDOM_REG32;
+      #elif defined(ESP32)
+        _id = esp_random();
+      #endif
+    };
+    bool isChanged() {
+      return _changed;
+    };
+    int getId() const {
+      return _id;
+    };
+    virtual ~Widget() = default;
+
+    friend class Tab;
+};
